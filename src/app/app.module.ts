b3ea66093c81e5425e65ff23ea949dd85d3a3be2
Benjamin Streit <benjaminmstreit@gmail.com>
2020-09-14 21:30:55 -0500

    Adding Home Menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeMenuComponent } from './home-menu/home-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeMenuComponent
   ],
   imports: [
     BrowserModule,
